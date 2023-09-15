56fa567b915902c15d1f12c082700e7448b76c79
Lokesh Arya <70194872+lokesh0177@users.noreply.github.com>
2023-09-15 16:19:04 +0530

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -4,7 +4,7 @@ import paralleldots
 # Setting your API key
 class API:
     def __init__(self):
-        paralleldots.set_api_key('6XV8Jg1iD3OBs1ZbA0i55zyWunZXHQGbDQeQnbBfdjs')
+        paralleldots.set_api_key('')
 
     def sentiment_analysis(self,text):
         response = paralleldots.sentiment(text)
